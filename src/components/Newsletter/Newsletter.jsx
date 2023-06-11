5df738f27e709b87d7467de0df4cf72c9296dfeb
Nicolas <nick2003@email.cz>
2023-06-11 22:15:53 +0200

    finalni upravy prvni page

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -4,9 +4,11 @@ import "./Newsletter.css"
 
 const Newsletter = () => {
 
-  function disappearFunction(){
+  function disappearFunction(event){
+    event.preventDefault()
     document.getElementById("new-element").style.display="block";
-    document.getElementById("bg-image__newsletter").style.display="none";
+    document.getElementById("bg-image__newsletter").style.opacity="0";
+    
   }
   return (
     <section className='newsletter'>
@@ -14,10 +16,10 @@ const Newsletter = () => {
         <div id="bg-image__newsletter">
             <div id="newsletter-wrapper">
                 <div className="newsletter-text">Subscribe to get <span className='colored'>50%</span> discount price</div>
-                <div className="input-wrapper">
-                  <input className='newsletter-input' type="email" id='email' placeholder='Email Address'/>
-                  <button className='newsletter-btn' onClick={disappearFunction}>Order now</button>
-                </div>
+                <form onSubmit={disappearFunction} className="input-wrapper">
+                  <input className='newsletter-input' required type="email" id='email' placeholder='Email Address'/>
+                  <input type="submit" className='newsletter-btn' value="Order Now"  />
+                </form>
           </div>
         </div>
           <div id="new-element">
