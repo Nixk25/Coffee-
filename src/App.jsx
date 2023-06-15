3f8726a44cc58c50886a9ba4dde9962add9108af
Nicolas <nick2003@email.cz>
2023-06-15 19:21:52 +0200

    uprava scrollovani a instal ikon

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Main from "./components/Main-page/Main"
 import Contact from "./components/Contact/Contact"
 import AboutPage from "./components/AboutPage/AboutPage"
+import ScrollToTop from './ScrollToTop'
+
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Main/>}/>
         <Route path='/About' element={<AboutPage/>}/>
