4e7c7d50983dab11b0ee78c250287ff3755333a4
Nicolas <nick2003@email.cz>
2023-06-27 17:47:04 +0200

    uprava footeru

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  return <div>Contact</div>;
+};
 
-export default Contact
+export default Contact;
